0c647f2758fdc84875ea5d4e426d263e1ec38e48
Rufus Pollock <rufus.pollock@okfn.org>
2014-05-25 21:49:40 +0100

    [all][m]: improve script in variety of ways.
    * correct script to use changed nationwide source url
    * Pull out percentage changes
    * Round numbers to correct decimal places
    * Change to iso formatted dates rather than float date

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ UK house prices since 1953 as monthly time-series. Data comes from the Nationwid
 
 Source: <http://www.nationwide.co.uk/hpi/historical.htm>
 
+## Data
+
+Data can be found in the `data/data.csv` file. See datapackage.json for
+source info.
+
+### Notes
+
+From the source XLS file (notes tab):
+
+> "The Nationwide house price methodology has developed over time and this
+  needs to be considered when interpreting the long run series of house
+  prices.  Maintenance in terms of updating weights for the mix-adjustment
+  process is carried out at regular intervals.  Significant developments
+  include:"
+> * 1952 - 1959 Q4 Simple average of purchase price.
+> * 1960 Q1 - 1973 Q4  - weighted average using floor area (thus allowing
+>   for the influence of house size).
+> * 1974 Q1 - 1982 Q4 - weighted averages using floor area, region and
+>   property type.
+> * 1983 Q1 -  Development of new house price methodology.  A statistical
+>   'regression' technique was introduced under guidance of ‘Fleming and
+>   Nellis’ (Loughborough University and Cranfield Institute of Technology).
+>   This was introduced in 1989 but data was revised back to 1983 Q1.
+> * 1993 - Information about neighbourhood classification (ACORN) used in
+>   the model were significantly updated following Census 1991 publication -
+>   regular updates since but typically for new postcodes.
+
+## Preparation
+
+Run:
+
+    python data.py process
+
--- data.py
+++ data.py
@@ -14,7 +14,7 @@ import datautil.tabular as T
 class Parser(object):
 
     def download(self):
-        nationwide = 'http://www.nationwide.co.uk/hpi/downloads/UK_house_price_since_1952.xls'
+        nationwide = 'http://www.nationwide.co.uk/~/media/MainSite/documents/about/house-price-index/downloads/uk-house-price-since-1952.xls'
         fp = 'cache/nationwide.xls'
         urllib.urlretrieve(nationwide, fp)
         self.localfps = [ fp ]
@@ -28,13 +28,29 @@ class Parser(object):
         tdata = reader.read()
         data = tdata.data
         out = T.TabularData()
-        out.header = [ 'Date', 'Price (All)', 'Price (New)', 'Price (Modern)', 'Price (Older)' ]
+        out.header = [ 'Date', 'Price (All)', 'Change (All)', 'Price (New)', 'Change (New)', 'Price (Modern)', 'Change (Modern)', 'Price (Older)' ]
         data = data[6:]
         data = zip(*data)
         def fix_date(indate): # e.g Q1 1952
             q,year = indate.split()
-            return int(year) + int(q[1])/4.0
-        data = [ [fix_date(x) for x in data[0]], data[2], data[5], data[8], data[11] ]
+            return '%s-%02d-01' % (int(year), (int(q[1])-1) * 3 + 2)
+        def fixup(series, decplaces=0):
+            out = [ round((x if (x not in ['', ' ']) else 0), decplaces) for x in series ]
+            if decplaces == 0:
+                out = [ int(x) for x in out ]
+            return out
+
+        data = [
+            [fix_date(x) for x in data[0]],
+            fixup(data[2]),
+            fixup(data[3], 1),
+            fixup(data[5]),
+            fixup(data[6], 1),
+            fixup(data[8]),
+            fixup(data[9], 1),
+            fixup(data[11]),
+            fixup(data[12], 1)
+            ]
         out.data = list(zip(*data))
         # outfp = 'data.js'
         # writer = T.JsonWriter()
@@ -42,6 +58,10 @@ class Parser(object):
         writer = T.CsvWriter()
         writer.write(out, open(outfp, 'w'))
         logger.info('Data successfully extracted to: %s' % outfp)
+
+    def process(self):
+        self.download()
+        self.extract()
 
 import optparse
 import inspect
